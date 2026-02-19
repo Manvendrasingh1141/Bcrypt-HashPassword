a7db540f65cf38d16f0d5830272338be5756762f
Manvendrasingh1141 <manvendrasinghshekhawat1141@gmail.com>
2026-02-19 20:48:05 +0530

    api routes updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static(join(__dirname,"Public")));
 connectDB();
-app.use("/api",router);
+app.use("/",router);
 
 app.use("/",(req,res)=>{
     res.sendFile(join(__dirname,"Public","home.html"));
